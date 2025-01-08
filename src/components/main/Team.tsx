409a3a2395830f3724e07c49f3e4932df0741d9e
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-01-08 15:53:52 +0500

    finished landing part

--- src/components/main/Team.tsx
+++ src/components/main/Team.tsx
@@ -6,38 +6,45 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import styles from '../../css/team.module.css'
 import fam from '../../images/fam.png'
+import { Link } from 'react-router-dom'
 
 type TProps = {}
 
 const Team: FC<TProps> = (props) => {
         return (
-                <section className={styles.team}>
+                <section className={`${styles.team} mainMarginTop`}>
                         <div className={`${styles.teamContainer} container`}>
-                                <h1 className='pageHeading'>Команда мечты Womazing</h1>
+                                <h1 className={`pageHeading ${styles.team__heading}`}>Команда мечты Womazing</h1>
                                 <Swiper
                                         cssMode={true}
                                         navigation={true}
                                         pagination={true}
                                         mousewheel={true}
                                         keyboard={true}
-                                        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                                        autoplay={true}
+                                        modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
                                         className={styles.swiper}
                                 >
                                         <SwiperSlide className={styles.swiperSlide}>
                                                 <img src={fam} alt='family' />
                                         </SwiperSlide>
-                                        <SwiperSlide className={styles.swiperSlide}>Slide 2</SwiperSlide>
-                                        <SwiperSlide className={styles.swiperSlide}>Slide 3</SwiperSlide>
+                                        <SwiperSlide className={styles.swiperSlide}>
+                                                <img src={fam} alt='family' />
+                                        </SwiperSlide>
+                                        <SwiperSlide className={styles.swiperSlide}>
+                                                <img src={fam} alt='family' />
+                                        </SwiperSlide>
                                 </Swiper>
                                 <div className={styles.teamText}>
-                                        <div className='teamText__heading'>
+                                        <div className={styles.teamText__heading}>
                                                 <h3>Для каждой</h3>
                                         </div>
-                                        <div className='teamText__description'>
-                                                <p>
-                                                        Каждая девушка уникальна. Однако, мы схожи в миллионе мелочей. Womazing ищет эти мелочи и создает прекрасные вещи, которые выгодно подчеркивают
-                                                        достоинства каждой девушки.
-                                                </p>
+                                        <div className={styles.teamText__description}>
+                                                <p>Каждая девушка уникальна. Однако, мы схожи в миллионе мелочей.</p>
+                                                <p>Womazing ищет эти мелочи и создает прекрасные вещи, которые выгодно подчеркивают достоинства каждой девушки.</p>
+                                        </div>
+                                        <div className={styles.teamText__button}>
+                                                <Link to={'/about'} className='linkButton'>Подробнее о бренде</Link>
                                         </div>
                                 </div>
                         </div>
